7206c6370890eba376814bb55bcc3626b8820c44
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-01-31 18:53:40 +0300

    auth

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './scheduleCard.dart';
+import '../dashBoard/TopBar.dart';
 
 
 class Schedules extends StatelessWidget {
@@ -14,15 +15,24 @@ class Schedules extends StatelessWidget {
             //     categories: "Schedules",
             //     iconData: Icons.Schedules_active_outlined,
             //     number_of_doctors: "fmdk"),
+            SizedBox(height: 20,),
+             TopBar(
+                  categories: "HAKIME",
+                  iconData: Icons.notifications_active_outlined,
+                  number_of_doctors: "fmdk"),
             SizedBox(height: 20.0),
             ScheduleCard(
                 person: "abebe fkroekf",
                 iconData: Icons.phone,
-                discription: "appointement was schedule for 10:00 am"),
+                discription: "appointement was schedule for 10:00 am",
+                time:new DateTime(1,1,1,1) ),
+
             ScheduleCard(
                 person: "jhon doe",
                 iconData: Icons.phone,
-                discription: "10")
+                discription: "10",
+                time:new DateTime(1,1,1,1) 
+                )
           ],
         ),
       ),
--- lib/schedule/scheduleCard.dart
+++ lib/schedule/scheduleCard.dart
@@ -5,10 +5,12 @@ class ScheduleCard extends StatelessWidget {
   final IconData iconData;
   final String person;
   final String discription;
+  final DateTime time;
   ScheduleCard(
       {required this.iconData,
       required this.person,
-      required this.discription});
+      required this.discription,
+      required this.time});
 
   Widget build(BuildContext context) {
     return Center(
@@ -16,7 +18,7 @@ class ScheduleCard extends StatelessWidget {
         margin: EdgeInsets.symmetric(vertical: 10.0),
         padding: const EdgeInsets.all(20.0),
         width: 450,
-        height: 170,
+        height: 110,
         decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
@@ -29,99 +31,53 @@ class ScheduleCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
             color: Colors.white,
             border: const Border()),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            Row(
+            Icon(Icons.calendar_month_outlined,size: 40,color: Colors.lightBlue.withOpacity(0.5),),
+            SizedBox(width: 10,),
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        person,
-                        style: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
-                            decoration: TextDecoration.none),
-                      ),
-
-                      // alignment: AlignmentGeometry alignment = Alignment.topLeft,
-                      Text(
-                        discription,
-                        style: TextStyle(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w200,
-                            color: Colors.grey[500],
-                            decoration: TextDecoration.none),
-                        textAlign: TextAlign.left,
-                      ),
-                    ]),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            person,
+                            style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black,
+                                decoration: TextDecoration.none),
+                          ),
+            
+                          // alignment: AlignmentGeometry alignment = Alignment.topLeft,
+                          Text(
+                            discription,
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.grey[500],
+                                decoration: TextDecoration.none),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            time.toString(),
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.grey[500],
+                                decoration: TextDecoration.none),
+                            textAlign: TextAlign.left,
+                          ),
+                        ]),
+                  ],
+                ),
               ],
             ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.green),
-                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10.0), // Adjust the radius as needed
-                          ),
-                        ),
-                      ),
-                      onPressed: () {
-                        // Handle button press
-                      },
-                      child: Column(
-                        children: [
-                          Icon(iconData, color: Colors.white, size: 30),
-                          Text(
-                            "Call",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300),
-                          ),
-                        ],
-                      ),
-                    )),
-                Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: ElevatedButton(
-                       style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
-                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10.0), // Adjust the radius as needed
-                          ),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Icon(iconData, color: Colors.white, size: 30),
-                          Text(
-                            "Cancel",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300),
-                          ),
-                        ],
-                      ),
-                    )
-                    ),
-                const SizedBox(width: 40),
-              ],
-            )
           ],
         ),
       ),
