9d2a0a74a0aa80db9f1b10507481a7ffe987805a
Yab <yabuman46@gmail.com>
2024-02-02 15:19:54 +0300

    feat: Schedule integration
    Integrated the schedule to work alongwith the backend. It loads existing schedules of a user

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -1,30 +1,83 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../utils/session.dart';
 import './scheduleCard.dart';
 import '../dashBoard/TopBar.dart';
 
-class Schedules extends StatelessWidget {
-  // Sample data representing schedules
-  final List<Map<String, dynamic>> schedulesData = [
-    {
-      'person': 'abebe fkroekf',
-      'iconData': Icons.phone,
-      'description': 'Appointment was scheduled for 10:00 am',
-      'time': DateTime(2024, 1, 31, 10, 0),
-    },
-    {
-      'person': 'john doe',
-      'iconData': Icons.phone,
-      'description': 'Some description here',
-      'time': DateTime(2024, 1, 31, 12, 30),
-    },
-    {
-      'person': 'john doe',
-      'iconData': Icons.phone,
-      'description': 'Some description here',
-      'time': DateTime(2024, 1, 31, 12, 30),
-    },
-    // Add more schedule data as needed
-  ];
+class Schedules extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ScheduleState();
+  }
+}
+
+class ScheduleState extends State<Schedules> {
+  List<Map<String, dynamic>> schedules = [];
+
+  @override
+  initState() {
+    super.initState();
+    loadSchedules();
+  }
+
+  Future<void> loadSchedules() async {
+    String userId = Session.state["userId"] ?? "None";
+    print("User id : $userId");
+    try {
+      String scheduleEndpoint = 'http://localhost:5072/api/user/schedule';
+
+      final response = await Session.get(scheduleEndpoint);
+      Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+      print(decodedResponse);
+      if (response.statusCode == 200) {
+        setState(() {
+          List<Map<String, dynamic>> temp =
+              (decodedResponse["schedule"] as List)
+                  .cast<Map<String, dynamic>>();
+          print(temp);
+          schedules = temp;
+        });
+        print('Message loading successful. Found ${schedules.length} messages');
+      } else {
+        print("Error while loading messages");
+        print(decodedResponse["errors"] ??
+            decodedResponse["error"] ??
+            "Unknown error occurred $decodedResponse");
+      }
+    } catch (error) {
+      // print('Something went wrong: $error');
+      rethrow;
+    }
+  }
+
+  // Future<void> detailSchedule(String message) async {
+  //   try {
+  //     String userId = Session.state["userId"] ?? "None";
+  //     String chattingEndpoint = 'http://localhost:5072/api/user/$userId/chat';
+  //     print("Asking llm with message: $message");
+  //     final response = await Session.post(chattingEndpoint, message);
+
+  //     Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+  //     print(decodedResponse);
+
+  //     if (response.statusCode == 200) {
+  //       setState(() {
+  //         aiMessage = decodedResponse["response"];
+  //       });
+  //       print("Ai response: $aiMessage");
+  //     } else {
+  //       print('Ai response failed');
+  //       print(decodedResponse["errors"] ??
+  //           decodedResponse["error"] ??
+  //           "Unknown error occurred $decodedResponse");
+  //     }
+  //   } catch (error) {
+  //     // print('Error: $error');
+  //     rethrow;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +93,16 @@ class Schedules extends StatelessWidget {
             ),
             SizedBox(height: 20.0),
             // Use the map function to create a list of ScheduleCard widgets
-            ...schedulesData.map((schedule) {
+            ...schedules.map((schedule) {
+              DateTime dateTime = DateTime.parse(schedule['scheduleTime']);
+              String formattedDate = DateFormat("yyyy-MM-dd").format(dateTime);
+              DateTime currentDate = DateTime.now();
+              int daysDifference = dateTime.difference(currentDate).inDays;
               return ScheduleCard(
-                person: schedule['person'],
-                discription: schedule['description'],
-                time: schedule['time'],
+                person: schedule['doctor']['fullname'],
+                discription: schedule['doctor']['specialization'],
+                time: "Date $formattedDate",
+                remainingDays: "In $daysDifference days",
               );
             }).toList(),
           ],
