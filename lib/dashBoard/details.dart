f383bedb34e6cbaabe9b28175e3dcc842ad11f3f
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-02-03 15:50:46 +0300

    schedule scroll

--- lib/dashBoard/details.dart
+++ lib/dashBoard/details.dart
@@ -32,23 +32,43 @@ class Details extends StatefulWidget {
 class _DetailsState extends State<Details> {
   DateTime? selectedDate;
   void _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2025),
-    ).then((value) {
-      if (value != null && DateTime.now().isBefore(value)) {
-        setState(() {
-          print("Date picked $value");
-          selectedDate = value;
-        });
+  showDatePicker(
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2000),
+    lastDate: DateTime(2025),
+  ).then((value) {
+    if (value != null && DateTime.now().isBefore(value)) {
+      setState(() {
+        print("Date picked $value");
+        selectedDate = value;
+      });
 
-        // Add your logic to send a request to the server with the selectedDate
-        _createSchedule(selectedDate);
-      }
-    });
-  }
+      // Add your logic to send a request to the server with the selectedDate
+      _createSchedule(selectedDate);
+
+      // Show an alert for successful schedule addition
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Success'),
+            content: Text('Schedule added successfully for $selectedDate.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the AlertDialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  });
+}
+
 
   Future<void> _createSchedule(DateTime? selectedDate) async {
     if (selectedDate != null) {
